031fc1aece7548776d304785a6cd3dd28e9562ee
Omarhmdn <jcsk0685@leeds.ac.uk>
2026-02-23 22:29:12 +0000

    worksheet done

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Omar Hamdan
+Student ID: 201972526
 */
 
 #include <stdio.h>
@@ -10,8 +10,16 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[256] = "";  
 
     // process the command-line data using appropriate string functions
+    if (argc > 1) {                     // at least one argument provided
+        strcpy(buffer, argv[1]);        // start with the first argument
+        for (int i = 2; i < argc; i++) {
+            strcat(buffer, "-");        // add hyphen before next argument
+            strcat(buffer, argv[i]);    // append the next argument
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
